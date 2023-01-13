8d311b400efedfdbe50895872b1623915e38fa2d
Sergio <sergioelmillo@hotmail.com>
2023-01-12 23:07:02 -0300

    commit 2

--- js/miArchivo.js
+++ js/miArchivo.js
@@ -1,69 +1,96 @@
 
-let producto = "";
-let cantidad = 0;
-let total = 0;
-let seguirComprando = false
-let precio = 0;
-let cantidadFinal = 0;
+let productos = [
+    {nombre:'remera', precio:1600},
+    {nombre:'musculosa', precio:1400},
+    {nombre:'pantalon', precio:2500},
+    {nombre:'short', precio:2100},
+    ];
+let carrito= [];
 
 
-do {            // do para que lo meta una sola vez //
-    producto = prompt ("¿Que queres comprar?, remera, pantalon, short o musculosa?", "Ej: remera");
-    
+    alert("Hola soy Sergio, Bienvenido a mi tienda!");
 
-            // pido los datos mediante un prompt ↑ //
+ let seleccion = prompt('desea comprar algun producto? si o no?')
 
-        if (producto === 'remera' || producto === 'musculosa' || producto === 'pantalon' || producto === 'short') {
-            
-        cantidad = parseInt(prompt ("¿Cuántos querés comprar?"));
-
-        
-        }
-        //else { alert("por favor elija una de las opciones especificadas en los ejemplos")}
-
-      
-    
-    switch (producto) {
-        case 'remera':
-            precio = 1600;
-            break;
-        case "musculosa":
-            precio = 1400;
-            break;
-        case "pantalon":
-            precio = 2500;
-            break;
-        case "short":
-            precio = 2100;
-            break;
-        default:
-            alert("por favor elija una de las opciones especificadas");  //1
-            precio= 0;
-            cantidad= 0;
-
-        
-    }
-
-    total += precio*cantidad;
-    cantidadFinal += cantidad;
-
-    seguirComprando = confirm("¿Querés agregar otro producto?");  //3
+while (seleccion != "si" && seleccion != "no") {
+    alert("por favor ingrese la opción si o no")
+    seleccion = prompt('desea comprar algun producto? si o no?')
 }
-while (seguirComprando); 
+
+if (seleccion == "si") {
+    alert("a continuacion voy a mostrarle una lista con nuestros productos")
+    let todosLosProdusctos = productos.map(
+        (productos) => productos.nombre + " " + productos.precio + "$"
+    );
+        alert(todosLosProdusctos.join(" - "))
+
+}   else if (seleccion == "no") {
+        alert("gracias por venir, hasta pronto!!")
+}
+
+
+while (seleccion !="no") {
     
+  let  productos = prompt ("¿Que queres comprar?, remera, pantalon, short o musculosa?", "elija una opción");
+    let precio = 0
 
+    if (productos === 'remera' || productos === 'musculosa' || productos === 'pantalon' || productos === 'short') {
 
-alert('Acaba de comprar '+cantidadFinal+' productos y el total es $'+total);
+          switch (productos) {
+         case 'remera':
+             precio = 1600;
+             break;
+         case "musculosa":
+             precio = 1400;
+             break;
+         case "pantalon":
+             precio = 2500;
+             break;
+         case "short":
+             precio = 2100;
+             break;
+         default:
+            alert("por favor elija una de las opciones especificadas"); 
+             precio= 0;
+             cantidad= 0;
 
-let finalizarCompra = confirm("¿Desea finalizar la compra?");
+     }
 
-if (finalizarCompra) alert("Su compra se ha realizado con éxito! muchas gracias por elegirnos");
+     let cantidad = parseInt(prompt ("¿Cuántos querés comprar?"))
 
-let envio= confirm('quiere usar nuestro servicio de envio?');
-if (envio) {
-    alert('el envio se le cobrara 500$ adicional' )
-    function envioo () {
-       return total + 500;
+     carrito.push({productos, cantidad, precio})
+     console.log(carrito)
+
+    } else {
+        alert('no tenemos ese producto')
     }
-    alert ('la tarifa total seria de $'+envioo())
-} else {'disculpe las molestias'};
+
+    seleccion= prompt('desea seguir comprando?')
+
+    while (seleccion === "no") {
+        alert('gracias por la compra! adios')
+        carrito.forEach((carritoFinal) => {
+            alert(`producto: ${carritoFinal.productos}, unidades: ${carritoFinal.cantidad},
+            el total a pagar es de ${carritoFinal.cantidad * carritoFinal.precio}`)
+        })
+        break;
+    }
+
+}
+
+const total = carrito.reduce((acumulador, el) => acumulador + el.precio * el.cantidad, 0)
+alert (`el total a pagar por su compra es: ${total}$` )
+
+            
+//  extension prettier       
+
+// let envio= confirm('quiere usar nuestro servicio de envio?');
+// if (envio) {
+//     alert('el envio se le cobrara 500$ adicional' )
+//     function envioo () {
+//        return total + 500;
+//     }
+//     alert ('la tarifa total seria de $'+envioo())
+// } else {'disculpe las molestias'};
+
+
