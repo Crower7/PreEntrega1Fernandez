04092df1f8066e7f3ca0991e570491ae46c76284
Sergio <sergioelmillo@hotmail.com>
2023-01-26 22:46:36 -0300

    commit 4/3

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const contenidoDeLaTienda = document.querySelector('#contenidoTienda')
 
 let productos = [
-    {id:1, nombre:'remera', precio:1600},
-    {id:2, nombre:'musculosa', precio:1400},
-    {id:3, nombre:'pantalon', precio:2500},
-    {id:4, nombre:'short', precio:2100},
+    {id:1, nombre:'remera', precio:1600, img: "https://vermediamanager.prod.ingecloud.com/MediaFiles/Topyed/2022/12/14/1903233.jpg" },
+    {id:2, nombre:'musculosa', precio:1400, img: "http://d3ugyf2ht6aenh.cloudfront.net/stores/001/312/577/products/musculosa-hombre-free-gris-melange21-c02f576bcec5529f3e16210411463196-640-0.jpg" },
+    {id:3, nombre:'pantalon', precio:2500, img: "https://alcatraz.com.ar/wp-content/uploads/2021/02/Pantalon-Ultra-Negro_0000s_0010_Pantalon-ultra2.jpg" },
+    {id:4, nombre:'short', precio:2100, img: "https://http2.mlstatic.com/D_NQ_NP_812841-MLA51815014779_102022-W.jpg" },
+    {id:5, nombre:'remera', precio:1800, img: "https://vermediamanager.prod.ingecloud.com/MediaFiles/Topyed/2022/12/14/1903233.jpg" },
+    {id:6, nombre:'musculosa', precio:1600, img: "http://d3ugyf2ht6aenh.cloudfront.net/stores/001/312/577/products/musculosa-hombre-free-gris-melange21-c02f576bcec5529f3e16210411463196-640-0.jpg" },
+    {id:7, nombre:'pantalon', precio:2700, img: "https://alcatraz.com.ar/wp-content/uploads/2021/02/Pantalon-Ultra-Negro_0000s_0010_Pantalon-ultra2.jpg" },
+    {id:8, nombre:'short', precio:2300, img: "https://http2.mlstatic.com/D_NQ_NP_812841-MLA51815014779_102022-W.jpg" },
     ];
 
 let carrito= [];
