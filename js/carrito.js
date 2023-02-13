292ab0b04d186145e407bd3206f2f2fa57bca821
Sergio <sergioelmillo@hotmail.com>
2023-02-12 22:58:58 -0300

    commit 11

--- js/carrito.js
+++ js/carrito.js
@@ -1,60 +1,78 @@
 const pintarCarrito = () => {
-    ventanaCarritoC.innerHTML= "";
-    ventanaCarritoC.style.display = "flex";
-    const ventanaCarrito = document.createElement("div");
-        ventanaCarrito.className = "ventanaCarritoCss";
-        ventanaCarrito.innerHTML = `
+  ventanaCarritoC.innerHTML = "";
+  ventanaCarritoC.style.display = "flex";
+  const ventanaCarrito = document.createElement("div");
+  ventanaCarrito.className = "ventanaCarritoCss";
+  ventanaCarrito.innerHTML = `
         <h1 class = "ventanaCarrito_Titulo">Carrito</h1>
         `;
-        ventanaCarritoC.append(ventanaCarrito);
+  ventanaCarritoC.append(ventanaCarrito);
 
-        const botonCerrarVentana = document.createElement ("h1");           //boton de cerrar 
-            botonCerrarVentana.innerText = "X";
-            botonCerrarVentana.className = "cerrarVentanaBoton";
+  const botonCerrarVentana = document.createElement("h1"); //boton de cerrar
+  botonCerrarVentana.innerText = "X";
+  botonCerrarVentana.className = "cerrarVentanaBoton";
 
-            botonCerrarVentana.addEventListener("click", () =>{
-                ventanaCarritoC.style.display = "none";
-            });
+  botonCerrarVentana.addEventListener("click", () => {
+    ventanaCarritoC.style.display = "none";
+  });
 
-            ventanaCarrito.append(botonCerrarVentana);
+  ventanaCarrito.append(botonCerrarVentana);
 
+  carrito.forEach((productos) => {
+    //contenido del carrito
 
-
-            carrito.forEach((productos) =>{     //contenido del carrito
-
-            let carritoContent = document.createElement("div")
-            carritoContent.className = "contenido_carrito"      
-            carritoContent.innerHTML =  `
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "contenido_carrito";
+    carritoContent.innerHTML = `
                 <h3>${productos.nombre}</h3>
                 <p>${productos.precio} $ </p>
-                <p> carrito: ${productos.cantidad}
+                <p> cantidad: ${productos.cantidad}
+                <p>total: ${productos.cantidad * productos.precio} </p>
             `;
-                ventanaCarritoC.append(carritoContent);
+    ventanaCarritoC.append(carritoContent);
+    console.log(carrito.length);
 
-                let eliminar = document.createElement("span");      //boton para eliminar el producto
-                eliminar.innerText=("❌")
-                eliminar.className="delete-product"
-                carritoContent.append(eliminar);
+    let eliminar = document.createElement("span"); //boton para eliminar el producto
+    eliminar.innerText = "❌";
+    eliminar.className = "delete-product";
+    carritoContent.append(eliminar);
 
-                eliminar.addEventListener("click",eliminarProducto);    //al hacer click se ejeuta la F (eliminarProducto)
-        });
+    eliminar.addEventListener("click", eliminarProducto); //al hacer click se ejeuta la F (eliminarProducto)
+  });
 
-        const totalCarrito = carrito.reduce((acu,el) => acu + el.precio, 0)     //acu-acumulador , el-cada producto
+  const totalCarrito = carrito.reduce(
+    (acu, el) => acu + el.precio * el.cantidad,
+    0
+  ); //acu-acumulador , el-cada producto
 
-        const total = document.createElement ("div");
-            total.className = "total-content ";
-            total.innerHTML = `total a pagar: ${totalCarrito} $ `;
-            ventanaCarritoC.append(total);      // total a la ventana carrito
-    };
+  const total = document.createElement("div");
+  total.className = "total-content ";
+  total.innerHTML = `total a pagar: ${totalCarrito} $ `;
+  ventanaCarritoC.append(total); // total a la ventana carrito
+};
 
-    verCarrito.addEventListener("click",pintarCarrito);
+verCarrito.addEventListener("click", pintarCarrito);
 
-        //eliminar productos del carrito
-    const eliminarProducto = () =>{
-        const founId = carrito.find((element) => element.id);
+//eliminar productos del carrito
+const eliminarProducto = () => {
+  const founId = carrito.find((element) => element.id);
 
-        carrito = carrito.filter((carritoId) =>{
-            return carritoId !== founId;
-        });
-        pintarCarrito();
-    };
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== founId;
+  });
+  carritoContador();
+  carritoStorage();
+  pintarCarrito();
+};
+
+
+const carritoContador = () => {
+    cantidadCarrito.style.display = "block";
+
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
+
+carritoContador();
--- main.js
+++ main.js
@@ -1,48 +1,54 @@
-const contenidoDeLaTienda = document.querySelector('#contenidoTienda');
-const verCarrito = document.querySelector('#verCarrito');
-const ventanaCarritoC = document.querySelector('#ventanaCarritoContenedor');
+const contenidoDeLaTienda = document.querySelector("#contenidoTienda");
+const verCarrito = document.querySelector("#verCarrito");
+const ventanaCarritoC = document.querySelector("#ventanaCarritoContenedor");
+const cantidadCarrito = document.querySelector("#cantidadCarrito");
 
-
-let carrito= [];
-
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];    // recupera los elementos del localStorage 
 
 productos.forEach((productos) => {
-    let content = document.createElement("div"); // crear un div
-        content.className = "div_ropa";     // class a cada div para el css
+  let content = document.createElement("div"); // crear un div
+  content.className = "div_ropa"; // class a cada div para el css
 
-        content.innerHTML = `                        
+  content.innerHTML = `                        
         <img src="${productos.img}">
         <h3>${productos.nombre}</h3>
         <p class= "parrafo" >${productos.precio} $ </p>
         
     `;
 
-    contenidoDeLaTienda.append(content);
+  contenidoDeLaTienda.append(content);
 
-    let comprar = document.createElement("button"); // crear un boton 
-        comprar.innerText = "Comprar";
-        comprar.className = "shakeFix";
+  let comprar = document.createElement("button"); // crear un boton
+  comprar.innerText = "Comprar";
+  comprar.className = "shakeFix";
 
-            content.append(comprar);
+  content.append(comprar);
 
-    comprar.addEventListener("click", () => {
-        const noRepeat = carrito.some((productoRepetido) => productoRepetido.id === productos.id);
-            if (noRepeat === true) {
-                carrito.map((prod)=>{
-                    if(prod.id === productos.id) {
-                        prod.cantidad++;
-                    }
-                });
-            } else {
-
-        carrito.push({
-            nombre: productos.nombre,
-            precio: productos.precio,
-            id: productos.id,
-            cantidad: productos.cantidad,
-        });
+  comprar.addEventListener("click", () => {
+    const noRepeat = carrito.some(
+      (productoRepetido) => productoRepetido.id === productos.id
+    );
+    if (noRepeat === true) {
+      carrito.map((prod) => {
+        if (prod.id === productos.id) {
+          prod.cantidad++;
+        }
+      });
+    } else {
+      carrito.push({
+        nombre: productos.nombre,
+        precio: productos.precio,
+        id: productos.id,
+        cantidad: productos.cantidad,
+      });
     }
-        console.log(carrito); 
-    });
+    console.log(carrito);
+    carritoContador();
+    carritoStorage();
+  });
 });
+
+const carritoStorage = () => {
+localStorage.setItem("carrito",JSON.stringify(carrito));    //me pasa el array de objeto del carrito en string al localStorage
+}
 
